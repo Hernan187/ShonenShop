43ce59c005f4d6daf4607e705a912cfce1f56b03
Hernan Jedlinski <checklinski@gmail.com>
2023-07-20 17:28:02 -0300

    creating the my new web application based on otaku staff

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar';
+
+const App = () => {
+    return (
+        <>
+            <Navbar/>
+
+            <h1 id='title'> Heeello NAKAMAAA </h1>
+        </>
+    );
+}
+
+export default App;
